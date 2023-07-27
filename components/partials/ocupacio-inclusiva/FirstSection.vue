420495da5b92e5ca2c79432560796a13b834d5e0
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-07-27 22:46:51 +0100

    orupacio inclusiva page

---
+++ components/partials/ocupacio-inclusiva/FirstSection.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class=" w-full ">
+        <div class=" w-full max-w-[850px] mx-auto py-20 px-4 xl:px-0">
+            <div class=" w-full flex sm:flex-row flex-col items-center space-y-20 sm:space-y-0 sm:justify-between">
+                <div class=" flex flex-col text-center space-y-2">
+                    <span class=" text-Ocre font-medium leading-[3.75rem] font-reenie-beanie text-[8rem]">179</span>
+                    <span class=" text-Ocre font-medium leading-[2.5rem] font-reenie-beanie text-[4rem]">persones</span>
+                </div>
+                <div class="  sm:max-w-[26.625rem] w-full">
+                    <p class=" text-lg font-normal text-black font-super-grotesk-ot">
+                        La Fundació Catalunya La Pedrera té un fort compromís amb les persones en situacions laborals, socials i econòmiques vulnerables. Des d'aquesta dedicació, la Fundació promou l'accés a l'ocupació com una eina prioritària per millorar les seves condicions de vida i construir un futur millor.
+                    </p>
+                </div>
+
+             
+            </div>
+        </div>
+
+        <div class=" w-full max-w-[827px] mx-auto py-20 ">
+            <div class=" w-full flex sm:flex-row flex-col space-y-16 sm:space-x-10 xl:space-x-0 sm:space-y-0 items-start sm:justify-between">
+                <div class=" w-full max-w-[37.5rem] h-[40rem] space-y-2 items-start flex flex-col">
+                    <span class=" text-lg text-black font-normal font-super-grotesk-ot">Creixement de l’ocupació inclusiva:</span>
+                    <h1 class=" text-xl font-bold text-black font-super-rotesk-comp">LLOCS DE TREBALL COOPERATIVA MANS</h1>
+                    <div class=" w-full h-[16.46rem] bg-gray-500 sm:max-w-[20.62rem]">
+
+                    </div>
+                   <div class=" w-full space-y-2 pt-4">
+                        <div class=" w-full flex flex-wrap  items-center  gap-5">
+                            <div class=" flex items-center space-x-1">
+                                <div class=" w-[0.875rem] h-[0.875rem] bg-Beige"></div>
+                                <span class=" text-[0.625rem] text-black font-normal font-super-grotesk-ot">Molí d’en Puigvert</span>
+                            </div>
+                            <div class=" flex items-center space-x-1">
+                                <div class=" w-[0.875rem] h-[0.875rem] bg-Ocre"></div>
+                                <span class=" text-[0.625rem] text-black font-normal font-super-grotesk-ot">Topromi</span>
+                            </div>
+                            <div class=" flex items-center space-x-1">
+                                <div class=" w-[0.875rem] h-[0.875rem] bg-Brown"></div>
+                                <span class=" text-[0.625rem] text-black font-normal font-super-grotesk-ot">MANS</span>
+                            </div>
+                            <div class=" flex items-center space-x-1">
+                                <div class=" w-[0.875rem] h-[0.875rem] bg-Turquoise"></div>
+                                <span class=" text-[0.625rem] text-black font-normal font-super-grotesk-ot">Horts de Sant Benet</span>
+                            </div>
+                        </div>
+                        <div class=" w-full flex flex-wrap  items-center  gap-5 justify-center">
+                          <div class=" flex items-center space-x-1">
+                            <div class=" w-[0.875rem] h-[0.875rem] bg-Olive-Green"></div>
+                            <span class=" text-[0.625rem] text-black font-normal font-super-grotesk-ot">Personal d’Estructura</span>
+                        </div>
+                        </div>
+                   </div>
+                </div>
+                <div class=" w-full max-w-[27.5rem] space-y-8 border-t border-black px-4 xl:px-0">
+                    <h1 class=" text-black text-[2rem] font-bold font-super-rotesk-comp leading-normal pt-4">COOPERATIVA MANS</h1>
+                    <p class="  text-black text-lg font-normal font-super-grotesk-ot ">
+                       Mitjançant la Cooperativa MANS, la Fundació treballa per generar oportunitats laborals i facilitar l’accés al món laboral per a joves en risc d’exclusió. Aquesta cooperativa es dedica a la producció, comercialització, envasat i transformació de fruita i verdura ecològica. <br> <br>
+                       Al llarg del 2022, un total de 32 persones en risc d’exclusió van treballar a la Cooperativa MANS, i 12 treballadors d’inserció van aconseguir l’accés a empreses ordinàries. Aquests resultats són especialment positius tenint en compte les dificultats econòmiques derivades del conflicte a Ucraïna i l’encariment de preus. MANS ha consolidat la seva relació amb els agricultors locals mitjançant un projecte de certificació conjunta que abasta tots els processos, des del camp fins a l’entrega del producte als supermercats. A més, durant el 2022, s’ha incrementat la producció elaborada a l’obrador de la cooperativa.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ pages/ocupacio-inclusiva/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HeroSection />
+       <div  class="relative overflow-hidden">
+         <FirstSection />
+       </div>
+        <div class="relative">
+            <SecondSection />
+            <img class="z-10 hidden md:block opacity-70 absolute top-96 right-[84%] xl:right-auto xl:left-0" 
+                src="/images/ocupacio-inclusiva/bg-vector-2.svg" alt="">
+                <img class="z-10 hidden md:block opacity-70 absolute top-[35rem] left-[86%] xl:left-auto xl:right-0" 
+                src="/images/ocupacio-inclusiva/bg-vector-1.svg" alt="">
+        </div>
+    </div>
+</template>
+<script setup>
+import HeroSection from "@/components/partials/ocupacio-inclusiva/HeroSection.vue";
+import FirstSection from "@/components/partials/ocupacio-inclusiva/FirstSection.vue";
+import SecondSection from "@/components/partials/ocupacio-inclusiva/SecondSection.vue";
+</script>
