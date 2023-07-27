116cefea3b9dc9e8b91225e907f24f535d07a1b5
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-07-27 17:24:32 +0100

    made the home page circle backgrounds

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <template>
     <div>
-        <!-- <div>
+        <div>
              <img src="" alt="" srcset=""> 
-        </div> -->
+        </div>
 
         <HeroSection />
-        <SecondSection />
-        <ThirdSection />
-        <FourthSection />
-        <FifthSection />
-        <SixthSection />
+        <div class="relative overflow-hidden flex flex-col">
+            <SecondSection class="z-0"/>
+            <ThirdSection class="z-0"/>
+            <FourthSection class="z-0"/>
+            <img class="z-10 hidden md:block opacity-70 absolute -top-36 left-[86%] xl:left-auto xl:right-0" src="/images/home/home-background-1.svg" alt="">
+        </div>
+        <div class="relative overflow-hidden flex flex-col">
+            <FifthSection />
+            <SixthSection />
+            <img class="z-10 hidden md:block opacity-70 absolute top-0 right-[84%] xl:right-auto xl:left-0" src="/images/home/home-background-2.svg" alt="">
+
+        </div>
+
         <SeventhSection />
 
     </div>
