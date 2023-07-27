27e5947c058fdc38b0f54caaa83b3a0253805e57
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-07-27 22:06:11 +0100

    envelliment digne

---
+++ pages/envelliment-digne/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HeroSection />
+       <div  class="relative overflow-hidden">
+         <FirstSection />
+          <img class="z-10 hidden md:block opacity-70 absolute top-48 left-[86%] xl:left-auto xl:right-0" 
+                src="/images/alimentacio/bg-vector-1.svg" alt="">
+       </div>
+        <div class="relative">
+            <SecondSection />
+            <img class="z-10 hidden md:block opacity-70 absolute top-[50rem] right-[84%] xl:right-auto xl:left-0" 
+                src="/images/alimentacio/bg-vector-2.svg" alt="">
+        </div>
+    </div>
+</template>
+<script setup>
+import HeroSection from "@/components/partials/envelliment-digne/HeroSection.vue";
+import FirstSection from "@/components/partials/envelliment-digne/FirstSection.vue";
+import SecondSection from "@/components/partials/envelliment-digne/SecondSection.vue";
+</script>
