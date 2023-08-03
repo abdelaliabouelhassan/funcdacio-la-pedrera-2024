f427a114d6c14c7c98d0ba7862ef917dd4fc9c0f
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-08-03 15:06:07 +0100

    collabse bug fixed

--- plugins/collabsable.js
+++ plugins/collabsable.js
@@ -1,25 +1,34 @@
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.directive('collapsable', {
-      mounted (el) {
+      mounted (el, binding) {
         const collapsable_content = el.querySelectorAll('.collapsable-content');
         const collapse_btn = el.querySelector('.collapse-btn');
         const collapse_btn_content = collapse_btn.querySelector('.collapse-btn-content');
         const chevron = collapse_btn.querySelector('.collapse-btn-chevron');
+        const isOpen = el.getAttribute('data-open') !== null
+        
         
 
         //initialze height at first so that the animations will be smooth on the first time
         el.classList.add('expanded')
         collapsable_content.forEach(content => {
-          content.style.height = content.scrollHeight + 'px';
+          content.style.height='auto'
           content.style.height = 0;
         });
         el.classList.remove('expanded')
         
+        if(isOpen)
+        {
+          setTimeout(() => {
+            expand()
+          }, 1000);
+        }       
 
         function expand() {
           el.classList.toggle('expanded');
           collapsable_content.forEach(content => {
+           
             if (el.classList.contains('expanded')) {
               content.style.height = content.scrollHeight + 'px';
               if(chevron){
@@ -30,7 +39,7 @@ export default defineNuxtPlugin((nuxtApp) => {
               }
             } 
             else {
-              content.style.height = 0;
+              content.style.height = 0 + 'px';
               if(chevron){
                 chevron.classList.remove('-rotate-180')
               }
@@ -40,24 +49,26 @@ export default defineNuxtPlugin((nuxtApp) => {
             }
           });
         }
-        //expand();
         collapse_btn.addEventListener('click', () => {
           expand();
         });
 
         window.addEventListener('resize', () => {
-          el.classList.remove('expanded')
-          collapsable_content.forEach(content => {
-            content.style.height = 0;
-            if(chevron){
-              chevron.classList.remove('-rotate-180')
-            }
-            if(collapse_btn_content){
-              collapse_btn_content.textContent =  'Més'        
-            }
-          });
+          if(!isOpen){
+            el.classList.remove('expanded')
+            collapsable_content.forEach(content => {
+              content.style.height = 0;
+              if(chevron){
+                chevron.classList.remove('-rotate-180')
+              }
+              if(collapse_btn_content){
+                collapse_btn_content.textContent =  'Més'        
+              }
+            });
+          }
         });
       },
+      
       getSSRProps (binding, vnode) {
         // you can provide SSR-specific props here
         return {}
