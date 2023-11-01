5a1432ed772a2f6d161dbeba640009c714df0854
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-11-01 15:16:16 +0100

    svgs

--- pages/exposicions/index.vue
+++ pages/exposicions/index.vue
@@ -1,8 +1,20 @@
 <template>
     <div class=" w-full">
        <HeroSection />
-       <FirstSection />
-       <SecondSection />
+      <div  class="relative overflow-hidden">
+         <FirstSection />
+          <img class="z-10 hidden md:block opacity-70 absolute mix-blend-multiply top-48 left-[86%] xl:left-auto xl:right-0" 
+                src="/images/alimentacio/bg-vector-1.svg" alt="">
+       </div>
+       <div class="relative overflow-hidden">
+            <SecondSection />
+            <img class="z-10 hidden md:block opacity-70 absolute mix-blend-multiply top-20 right-[84%] xl:right-auto xl:left-0" 
+                src="/images/alimentacio/bg-vector-2.svg" alt="">
+                <img class="z-10 hidden md:block mix-blend-multiply opacity-70 absolute top-[49rem] left-[86%] xl:left-auto xl:right-0" 
+                src="/images/vocacio/bg-vector-1.svg" alt="">
+                <img class="z-10 hidden md:block mix-blend-multiply opacity-70 absolute bottom-96 right-[84%] xl:right-auto xl:left-0" 
+                    src="/images/vocacio/bg-vector-2.svg" alt="">
+        </div>
     </div>
 </template>
 
