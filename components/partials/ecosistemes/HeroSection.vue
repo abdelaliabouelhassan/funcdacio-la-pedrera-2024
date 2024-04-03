cee9d702e9831b0a6acd881c36875576fb481cd7
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-04-03 10:48:02 +0000

    change hero img

--- components/partials/ecosistemes/HeroSection.vue
+++ components/partials/ecosistemes/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="h-screen max-h-[46.94rem] relative flex justify-center items-center">
-        <img  class=" z-0 absolute top-0 left-0 h-full w-full object-cover object-bottom " src="/images/ecosistemes/hero-background.webp" alt="" srcset="">
+        <img  class=" z-0 absolute top-0 left-0 h-full w-full object-cover object-bottom " src="/images/ecosistemes/hero-background.png" alt="" srcset="">
         <div class="flex z-10 flex-col items-center gap-y-8 text-center px-4 text-white">
             <h1 class="text-[2.5rem] font-normal tracking-[0.25rem] leading-normal 
                         sm:text-[4rem]
